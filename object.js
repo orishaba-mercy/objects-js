752f6b32a473eaf7a29cd942a6dd0050089efd41
orishaba-mercy <orishabamercybanga@gmail.com>
2023-04-26 13:39:12 +0300

    changes

--- object.js
+++ object.js
@@ -1,19 +1,27 @@
 //  Given an array of objects, each object representing a person with a 
 // name and age property, write a function that returns a new array 
 // containing the names of all people who are 18 years old or older.
-const people = [
-  { name: 'Alice', age: 17 },
-  { name: 'Eunice', age: 22 },
-  { name: 'Charlie', age: 14 },
-  { name: 'Max', age: 19 },
-];
+// const people = [
+//   { name: 'Alice', age: 17 },
+//   { name: 'Eunice', age: 22 },
+//   { name: 'Charlie', age: 14 },
+//   { name: 'Max', age: 19 },
+// ];
 function humans(){
-  let adults=people.filter(function(people){
-    return people.age>=18;
-});
-   console.log(adults);
+  let adults=people.filter(person=>person.age>=18)
+
+   let nameOlder=adults.map(person=>person.name);
+return nameOlder
+
 }
-humans()
+const people = [
+   { name: 'Alice', age: 17 },
+    { name: 'Eunice', age: 22 },
+     { name: 'Charlie', age: 14 },
+     { name: 'Max', age: 19 },
+  ];
+ console.log(humans())
+ 
   
 
 
@@ -21,34 +29,81 @@ humans()
 //  represents a product with a name, price, and category property. 
 // The function should return an object that groups the products by their categories, 
 // with the category names as keys and the arrays of products as values.
-const products = [
-  { name: 'Laptop', price: 1200, category: 'Electronics' },
-  { name: 'Shirt', price: 25, category: 'Clothing' },
-  { name: 'Headphones', price: 80, category: 'Electronics' },
-  { name: 'Shoes', price: 60, category: 'Clothing' },
-];
-function object(params) {
-  
-}
-const result=products.reduce((group,arr)=>{
-  const{ category}=arr;
+// const products = [
+//   { name: 'Laptop', price: 1200, category: 'Electronics' },
+//   { name: 'Shirt', price: 25, category: 'Clothing' },
+//   { name: 'Headphones', price: 80, category: 'Electronics' },
+//   { name: 'Shoes', price: 60, category: 'Clothing' },
+// ];
+ function object() { 
+const result=products.reduce((group,array)=>{
+  const{ category}=array;
   group[category]=group[category] ?? []
-  group[category].push(arr);
+  group[category].push(array);
   return group;
 },
 {})
 console.log(result);
+ }
+ object()
+// // Given an array of objects, where each object represents 
+// // a student with a name and an array of scores, write a function 
+// // that returns a new array containing the names of all students whose
+// //  average score is greater than or equal to 85.
+// const students = [
+//   { name: 'John', scores: [90, 80, 85] },
+//   { name: 'Jane', scores: [95, 92, 88] },
+//   { name: 'Jim', scores: [70, 80, 75] },
+//   { name: 'Jill', scores: [85, 90, 84] },
+// ];
+const studentsScores=(students)=>{
+  const highestScore=[]
+  students.forEach(student => {
+    const{name,scores}=student;
+    const average=scores.reduce((sum,score)=>sum+score ,0)/scores.length;
+    if (average>=85){
+    highestScore.push(name);
+    }
+    
+  });
+  return highestScore;
+}
+  const students = [
+     { name: 'John', scores: [90, 80, 85] },
+       { name: 'Jane', scores: [95, 92, 88] },
+      { name: 'Jim', scores: [70, 80, 75] },
+      { name: 'Jill', scores: [85, 90, 84] },
+    ];
+    var x=studentsScores(students);
+    console.log(x);
 
-// Given an array of objects, where each object represents a student with a name and an array of scores, write a function that returns a new array containing the names of all students whose average score is greater than or equal to 85.
-const students = [
-  { name: 'John', scores: [90, 80, 85] },
-  { name: 'Jane', scores: [95, 92, 88] },
-  { name: 'Jim', scores: [70, 80, 75] },
-  { name: 'Jill', scores: [85, 90, 84] },
-];
 
-// Given an object representing a car, with properties for the make, model, year, and a method to display the car's information, write a function that takes the car object and adds a new method to the object called age. The age method should return the current age of the car based on the current year and the car's year property.
-const car = {
+
+// // Given an object representing a car, with properties for the make,
+// //  model, year, and a method to display the car's information, 
+// // write a function that takes the car object and adds a new method to the object
+// //  called age. The age method should return the 
+// // current age of the car based on the current year 
+// // and the car's year property.
+// const car = {
+//   make: 'Ford',
+//   model: 'Ranger',
+//   year: 2023,
+//   displayInfo: function() {
+//     console.log(`Make: ${this.make}, Model: ${this.model}, Year: ${this.year}`);
+//   },
+// };
+
+ 
+
+
+ const newmethod = (car)=>{
+  let currentyear=new Date().getFullYear();
+  car.age=function(){
+    return currentyear-this.year;
+  };
+ };
+ const car = {
   make: 'Ford',
   model: 'Ranger',
   year: 2023,
@@ -56,3 +111,7 @@ const car = {
     console.log(`Make: ${this.make}, Model: ${this.model}, Year: ${this.year}`);
   },
 };
+
+
+ newmethod(car);
+ console.log(car.age()) ;
